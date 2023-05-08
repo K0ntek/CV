12c23029dda17d6a5c50ab983bb21fd32bdfede5
Kuba <93491047+K0ntek@users.noreply.github.com>
2023-05-08 22:51:21 +0200

    Add files via upload

--- cv/src/components/footer.jsx
+++ cv/src/components/footer.jsx
@@ -7,10 +7,10 @@ const Footer=()=>{
     return(
        <div>
         <div className="w-full h-[10px] bg-gradient-to-r from-[#1b003d] to-[#42088f] "></div>
-         <div className="footer bg-[#05010a] py-[50px]">
+         <div className="footer bg-black py-[50px]">
             <div className="text-white text-center py-[10px] space-y-[20px]">
             <h1 className=" text-[40px] lg:text-[70px] font-orbitron">JAKUB KONTEK</h1>
-                    <div className="w-fit mx-auto">
+                    <div className="w-fit mx-auto h-[120px] sm:h-fit">
                                 <TypeAnimation
                             sequence={[
                                 'Front-end developer',
@@ -27,21 +27,21 @@ const Footer=()=>{
                             <div className="w-[50%] h-[4px] bg-[#4702a0] rounded-full mx-auto group-hover:w-[50%] transition-all duration-1000 delay-200"></div>
                     </div>
             </div>
-            <div className="flex space-x-[10px] text-white py-[5px] justify-center">
+            <div className="flex space-x-[10px] text-white py-[5px] pb-[20px] justify-center">
                 <a href="https://www.facebook.com/profile.php?id=100009894456441"><BsFacebook className="text-[30px] hover:text-[#1282f1] transition-all duration-300"/></a>
                 <a href="https://github.com/k0ntek"><BsGithub className="text-[30px] hover:text-[#1282f1] transition-all duration-300"/></a>
             </div>
-        </div>
-        <div className="bg-[#05010a] text-white text-[20px] text-center py-[20px] sm:flex space-y-[15px] md:space-y-0 space-x-[40px] justify-center">
+            <div className="bg-[#05010a] text-white text-[20px] text-center py-[20px] sm:flex space-y-[15px] md:space-y-0 space-x-[40px] justify-center">
            <p className="font-orbitron"> 790 839 086</p>
 
             <p className="font-orbitron">jakub.kontek05@gmail.com</p>
             </div>
-           <div className="bg-black">
+           <div className="bg-[#05010a] py-[20px]">
            <p className="w-[80%] mx-auto text-[14px] text-white font-gruppo font-bold">Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji zgodnie
                 z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób
                 fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO).</p>
            </div>
+        </div>
         </div>
     )
 }
